012a7f68bfdad5467410e32f438985ed3eb4e43d
wmath84-ux <wmath84@gmail.com>
2026-06-30 04:43:55 +0000

    Track external browser host state

--- app/browser/browserState.js
+++ app/browser/browserState.js
@@ -1,7 +1,10 @@
 export const browserState = {
   status: 'closed',
+  browser: null,
   context: null,
   launchingPromise: null,
+  hostProcess: null,
+  remoteDebuggingPort: null,
   lastError: null,
   profilePath: null
 };
